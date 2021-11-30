ac02719b29b4827885eade33a2c595b9fbd73eee
WlCampoamor <87621237+WlCampoamor@users.noreply.github.com>
2021-11-30 12:45:04 +0100

    Add files via upload

--- DHT11TermistorLCD.ino
+++ DHT11TermistorLCD.ino
@@ -5,25 +5,25 @@
 
  * El esquema electrico es el usado por ELEGOO tanto de termistor, DHT-11 y Display 16,2
  
- * el resto de modificaciones y adaptación del codigo han sido realizadas por Walter L.Campoamor Stursberg (2021).
+ * Rev. 2.0  20.NOV.2021  by  Walter L.Campoamor Stursberg.
  
- 
+ * 
  */
 
-#include <LiquidCrystal.h> // incluimos la libreria LCD
-#include <dht_nonblocking.h> // incluimos la libreria sensor de humedad y temperatura DHT-11 distribuido por Elegoo
-#define DHT_SENSOR_TYPE DHT_TYPE_11 // define el sensor DHT-11
-static const int DHT_SENSOR_PIN = 2; // declara el PIN 2 Digital como entero constante estatica de entrada DHT-11
-int tempPin = 0; //  PIN A0 analogico para lectura de datos del termistor distribuido por Elegoo
+#include <LiquidCrystal.h>                                      // incluimos la libreria LCD
+#include <dht_nonblocking.h>                                    // incluimos la libreria sensor de humedad y temperatura DHT-11 distribuido por Elegoo
+#define DHT_SENSOR_TYPE DHT_TYPE_11                             // define el sensor DHT-11
+static const int DHT_SENSOR_PIN = 2;                            // declara el PIN 2 Digital como entero constante estatica de entrada DHT-11, tambien podiamos declararlo como #define.
+int tempPin = 0;                                                //  PIN A0 analogico para lectura de datos del termistor distribuido por Elegoo
  
 //                BS  E  D4 D5  D6 D7
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12); // Declaración de los pines de la pantalla LCD 16-2
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);                   // Declaración de los pines de la pantalla LCD 16-2 para la bliblioteca.
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT); // declaro Pin 13 como salida
-  lcd.begin(16, 2); // declaro filas y columnas del display utilizado
+  pinMode(LED_BUILTIN, OUTPUT);                                  // declaro Pin 13 como salida
+  lcd.begin(16, 2);                                              // declaro filas y columnas del display utilizado
 }
 
 /*
@@ -73,23 +73,28 @@ int tempReading = analogRead(tempPin);
   /* Seccion DHT-11 */
    
     lcd.setCursor(0, 0);
-        lcd.print( temperature, 1 );
-    lcd.print(" C");
-    lcd.setCursor(8, 0);
-        lcd.print( humidity, 1 );
-    lcd.println( "%   " );
+    lcd.print("In:");
+    lcd.setCursor(4, 0);
+    lcd.print(temperature, 1 );
+    lcd.print((char)223);
+    lcd.print("C");
+    lcd.setCursor(11, 0);
+    lcd.print( humidity, 1 );
+    lcd.println( "%" );
    
    /* Sección termistor */
     
     lcd.setCursor(0, 1);
-    lcd.print("Temp         C ");
-    lcd.setCursor(6, 1);
+    lcd.print("Out:");
+    lcd.setCursor(4, 1);
     lcd.print(tempC);
-
+    lcd.print((char)223);
+    lcd.print("C");
+    
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) 
-delay(2000);
+    delay(150);
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(2000);
+    delay(150);
   }
   
 }
